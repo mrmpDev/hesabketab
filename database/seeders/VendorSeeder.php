eec9711688e18594938d8381ff2fb118575ae8f6
MRMP <mohamadpouran@gmail.com>
2026-09-01 22:53:47 +0330

    update theme color add new vendor to vendor list

--- database/seeders/VendorSeeder.php
+++ database/seeders/VendorSeeder.php
@@ -14,6 +14,7 @@ class VendorSeeder extends Seeder
     public function run(): void
     {
         $vendors = [
+            'فروشگاه پیش فرض',
             'داروخانه دل آذر',
             'لبنیات سقاخانه',
             'ساندویچ شبنم',
